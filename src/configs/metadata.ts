f2cd7dcc4b05d865352e99bbc90aa64d10b90f02
emiriko <notalvaroaustin@gmail.com>
2024-02-06 01:02:14 +0700

    feat: initialize repository

---
+++ src/configs/metadata.ts
@@ -0,0 +1,17 @@
+export const METADATA = {
+    title: {
+      default: 'Revelio',
+      template: '%s | Revelio',
+    },
+    description: 'Event Planner App',
+    icons: {
+      icon: ['favicon/favicon-32x32.png', 'favicon/favicon-16x16.png'],
+      apple: {
+        href: '/favicon/apple-touch-icon.png',
+        rel: 'apple-touch-icon',
+        url: '/',
+      },
+    },
+    manifest: '/favicon/site.webmanifest',
+    keywords: ['Event', 'Planner', 'App'],
+}
