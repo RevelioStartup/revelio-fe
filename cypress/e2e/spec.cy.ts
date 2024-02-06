bbb92f0946d70e9c5546ba8bad2b0cc136f8de4b
emiriko <notalvaroaustin@gmail.com>
2024-02-06 16:18:08 +0700

    feat: add cypress for testing

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,11 @@
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000")
+  })
+
+  it("the h3 contains the correct text", () => {
+    cy.get("h3").contains(
+      "Ease. Enhance. Commemorate."
+    )
+  })
+})
