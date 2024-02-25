513981c88366cc815e8eeee1a7f8b56a4d034068
Catherine Angel <catheriner40540@gmail.com>
2024-02-25 20:28:41 +0700

    [RED] add tests for sugesti ai element

---
+++ __tests__/sugesti-ai/sugesti-ai.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react'
+import { AIAside } from '../../src/app/plans/AISuggestion/AIAside'
+import '@testing-library/jest-dom'
+
+describe('Sugesti AI Component', () => {
+  it('renders the aside element', () => {
+    render(<AIAside />)
+    const asideElement = screen.getByTestId('ai-aside')
+    expect(asideElement).toBeInTheDocument()
+  })
+
+  it('renders an input that allows users to write their prompt', () => {
+    render(<AIAside />)
+    const aiInput = screen.getByTestId('ai-input')
+    expect(aiInput).toBeInTheDocument()
+    expect(aiInput).toHaveAttribute('placeholder', 'Ask AI to help planning your event')
+  })
+
+  it('renders a button for user confirming their prompt', () => {
+    render(<AIAside />)
+    const aiButton = screen.getByTestId('ai-button')
+    expect(aiButton).toBeInTheDocument()
+  })
+  it('renders examples for prompt to give user ideas', () => {
+    render(<AIAside />)
+    const promptExample = screen.getByTestId('prompt-example')
+    expect(promptExample).toBeInTheDocument()
+  })
+})
