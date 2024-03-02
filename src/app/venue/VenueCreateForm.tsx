a0f6d6acf4c365b1ac9adc59fd52f2006feaf0f0
Natania Deandra <nataniadeandra@gmail.com>
2024-03-02 19:44:44 +0700

    [RED] added tests for venue feature

---
+++ src/app/venue/VenueCreateForm.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+import { Box } from '@mui/material'
+
+type VenueCreateFormProps = {
+  onSubmit?: (data: any) => void
+}
+
+export const VenueCreateForm = () => {
+  return <div></div>
+}
+
+export default VenueCreateForm
