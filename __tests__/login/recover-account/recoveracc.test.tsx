f08b5a1a43ed59aaebfa4a385796eb2a67265fcf
JohannesSetiawan <88031010+JohannesSetiawan@users.noreply.github.com>
2024-03-02 08:42:32 +0700

    feat: Adding feature for Membuat Akun. (#12)
    What changes/added:
    
    Add page for Login and its tests
    Add page for Register and its tests
    Add page for recover account and its tests
    Add page for email verification and its tests
    Add authApi to call BE API and modify userSlice.ts

---
+++ __tests__/login/recover-account/recoveracc.test.tsx
@@ -0,0 +1,85 @@
+import { render, fireEvent, waitFor } from '@testing-library/react'
+import RecoverAccount from '@/app/login/recover-account/page'
+import { useSendChangePasswordMutation } from '@/redux/api/authApi'
+import '@testing-library/jest-dom'
+
+jest.mock('@/redux/api/authApi', () => ({
+  useSendChangePasswordMutation: jest.fn(),
+}))
+
+describe('Test for recover account page', () => {
+  beforeEach(() => {
+    const mockChangePassword = jest.fn().mockResolvedValue({ data: {} })
+    ;(useSendChangePasswordMutation as jest.Mock).mockReturnValue([
+      mockChangePassword,
+    ])
+  })
+
+  it('renders recover account form', () => {
+    const { getByTestId } = render(<RecoverAccount />)
+    expect(getByTestId('recover-form')).toBeInTheDocument()
+  })
+
+  it('submits recover form successfully', async () => {
+    const mockChangePassword = jest.fn().mockResolvedValue({ data: {} })
+    ;(useSendChangePasswordMutation as jest.Mock).mockReturnValue([
+      mockChangePassword,
+    ])
+    const { getByTestId } = render(<RecoverAccount />)
+    fireEvent.change(getByTestId('email-input'), {
+      target: { value: 'email@email.com' },
+    })
+    fireEvent.change(getByTestId('password-input'), {
+      target: { value: 'password' },
+    })
+    fireEvent.change(getByTestId('token-input'), {
+      target: { value: 'token' },
+    })
+    fireEvent.submit(getByTestId('recover-form'))
+    await waitFor(() => expect(mockChangePassword).toHaveBeenCalledTimes(1))
+  })
+
+  it('shows error message when recover account fails due to invalid token and/or email', async () => {
+    const errorMessage = 'Invalid token and/or email!'
+    const mockChangePassword = jest
+      .fn()
+      .mockResolvedValue({ error: { data: { msg: errorMessage } } })
+    ;(useSendChangePasswordMutation as jest.Mock).mockReturnValue([
+      mockChangePassword,
+    ])
+    const { getByTestId, getByText } = render(<RecoverAccount />)
+    fireEvent.change(getByTestId('email-input'), {
+      target: { value: 'email@email.com' },
+    })
+    fireEvent.change(getByTestId('password-input'), {
+      target: { value: 'password' },
+    })
+    fireEvent.change(getByTestId('token-input'), {
+      target: { value: 'token' },
+    })
+    fireEvent.submit(getByTestId('recover-form'))
+    await waitFor(() => expect(getByText(errorMessage)).toBeInTheDocument())
+    fireEvent.click(getByTestId('button-error'))
+  })
+
+  it('shows unkown error message when recover account fails due to unkown reason', async () => {
+    const errorMessage = 'Unknown Error!'
+    const mockChangePassword = jest.fn().mockResolvedValue({ error: {} })
+    ;(useSendChangePasswordMutation as jest.Mock).mockReturnValue([
+      mockChangePassword,
+    ])
+    const { getByTestId, getByText } = render(<RecoverAccount />)
+    fireEvent.change(getByTestId('email-input'), {
+      target: { value: 'email@email.com' },
+    })
+    fireEvent.change(getByTestId('password-input'), {
+      target: { value: 'password' },
+    })
+    fireEvent.change(getByTestId('token-input'), {
+      target: { value: 'token' },
+    })
+    fireEvent.submit(getByTestId('recover-form'))
+    await waitFor(() => expect(getByText(errorMessage)).toBeInTheDocument())
+    fireEvent.click(getByTestId('button-error'))
+  })
+})
---
+++ __tests__/register/verify/verify.test.tsx
@@ -0,0 +1,59 @@
+import { render, fireEvent, waitFor } from '@testing-library/react'
+import VerifyEmail from '@/app/register/verify/page'
+import { useVerifyEmailMutation } from '@/redux/api/authApi'
+import '@testing-library/jest-dom'
+
+jest.mock('@/redux/api/authApi', () => ({
+  useVerifyEmailMutation: jest.fn(),
+}))
+
+describe('Test for verify email page', () => {
+  beforeEach(() => {
+    const mockVerifyEmail = jest.fn().mockResolvedValue({ data: {} })
+    ;(useVerifyEmailMutation as jest.Mock).mockReturnValue([mockVerifyEmail])
+  })
+
+  it('renders verify email form', () => {
+    const { getByTestId } = render(<VerifyEmail />)
+    expect(getByTestId('verify-form')).toBeInTheDocument()
+  })
+
+  it('submits recover form successfully', async () => {
+    const mockVerifyEmail = jest.fn().mockResolvedValue({ data: {} })
+    ;(useVerifyEmailMutation as jest.Mock).mockReturnValue([mockVerifyEmail])
+    const { getByTestId } = render(<VerifyEmail />)
+    fireEvent.change(getByTestId('token-input'), {
+      target: { value: 'token' },
+    })
+    fireEvent.submit(getByTestId('verify-form'))
+    await waitFor(() => expect(mockVerifyEmail).toHaveBeenCalledTimes(1))
+  })
+
+  it('shows error message when verify email fails due to invalid token', async () => {
+    const errorMessage = 'Invalid token!'
+    const mockVerifyEmail = jest
+      .fn()
+      .mockResolvedValue({ error: { data: { msg: errorMessage } } })
+    ;(useVerifyEmailMutation as jest.Mock).mockReturnValue([mockVerifyEmail])
+    const { getByTestId, getByText } = render(<VerifyEmail />)
+    fireEvent.change(getByTestId('token-input'), {
+      target: { value: 'token' },
+    })
+    fireEvent.submit(getByTestId('verify-form'))
+    await waitFor(() => expect(getByText(errorMessage)).toBeInTheDocument())
+    fireEvent.click(getByTestId('button-error'))
+  })
+
+  it('shows unkown error message when verify email fails due to unkown reason', async () => {
+    const errorMessage = 'Unknown Error!'
+    const mockVerifyEmail = jest.fn().mockResolvedValue({ error: {} })
+    ;(useVerifyEmailMutation as jest.Mock).mockReturnValue([mockVerifyEmail])
+    const { getByTestId, getByText } = render(<VerifyEmail />)
+    fireEvent.change(getByTestId('token-input'), {
+      target: { value: 'token' },
+    })
+    fireEvent.submit(getByTestId('verify-form'))
+    await waitFor(() => expect(getByText(errorMessage)).toBeInTheDocument())
+    fireEvent.click(getByTestId('button-error'))
+  })
+})
