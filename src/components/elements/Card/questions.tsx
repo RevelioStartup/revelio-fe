8c3ee3cee88cb310a6c0b1d6540a3412f5dd6e62
swastikanata <swastika.nata1804@gmail.com>
2024-03-12 21:53:53 +0700

    [REFACTOR] fix code smell

--- src/components/elements/Card/questions.tsx
+++ src/components/elements/Card/questions.tsx
@@ -47,7 +47,6 @@ function Questions() {
             className="flex items-center justify-between hover:text-[#14b8a6] hover:text-bold"
             style={{ transition: '0.2s ease' }}
             onClick={() => handleToggle(item.id)}
-            
           >
             <button className="text-start font-bold leading-5">
               <Typography variant={'h6'} mb={1}>
