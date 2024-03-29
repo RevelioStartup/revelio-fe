6c988c61c5439103918a1132702749045bfc085e
JohannesSetiawan <johannes.setiawan@ui.ac.id>
2024-03-29 19:32:12 +0700

    [RED] Add test and APi for task details and steps.

---
+++ src/types/taskDetails.ts
@@ -0,0 +1,18 @@
+export type Step = {
+    id: string
+    name: string
+    output: string
+    description: string
+    status: string
+    step_order: number
+    task: string
+}
+
+export type Task = {
+    id: string
+    task_steps: Step[]
+    title: string
+    description: string
+    status: string
+    event: string
+}
