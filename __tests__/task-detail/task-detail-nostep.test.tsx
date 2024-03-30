d10ae4c207b47ecae223999a3c07fd9cc61bf17d
JohannesSetiawan <88031010+JohannesSetiawan@users.noreply.github.com>
2024-03-30 10:00:23 +0700

    Feat: Melihat step by step (API, Page, Testing) (#39)
    What changes:
    - Add redux API to fetch task details and steps
    - Add page for task details and steps
    - Add testing for task detail page

---
+++ __tests__/task-detail/task-detail-nostep.test.tsx
@@ -0,0 +1,62 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import TaskDetailPage from '@/app/event/[eventId]/(eventId)/[taskId]/page';
+import { useGetEventQuery } from '@/redux/api/eventApi';
+import { useGetTaskDetailQuery } from '@/redux/api/taskApi';
+import '@testing-library/jest-dom'
+
+jest.mock('@/redux/api/eventApi', () => ({
+    useGetEventQuery: jest.fn(),
+}))
+
+jest.mock('@/redux/api/taskApi', () => ({
+    useGetTaskDetailQuery: jest.fn(),
+}))
+
+
+describe('TaskDetailPage with no step', () => {
+    const mockEventData3 = {
+        id: '3',
+        name: 'event name',
+        budget: 'asd',
+        date: '2023-20-05',
+        objective: 'adfsf',
+        attendees: 'number',
+        theme: 'string',
+        services: 'string',
+    }
+
+    const mockTaskData3 = {
+        id: '3',
+        task_steps: [],
+        title: 'task title',
+        description: 'task description',
+        status: 'task status',
+        event: '3'
+    }
+
+    const mockGetEventQuery = useGetEventQuery as jest.Mock
+
+    mockGetEventQuery.mockReturnValue({
+        data: mockEventData3,
+        isLoading: false,
+    })
+
+    const mockGetTaskDetailQuery = useGetTaskDetailQuery as jest.Mock
+
+    mockGetTaskDetailQuery.mockReturnValue({
+        data: mockTaskData3,
+        isLoading: false
+    })
+
+  test('renders task details when not loading and data is available but no step', () => {
+
+    render(<TaskDetailPage params={{ eventId: '3', taskId: '3' }} />);
+
+    expect(screen.getByText('event name')).toBeInTheDocument();
+    expect(screen.getByText('task status')).toBeInTheDocument();
+    expect(screen.getByText('task description')).toBeInTheDocument();
+    expect(screen.getByText('task title')).toBeInTheDocument();
+    expect(screen.getByText('No steps created yet')).toBeInTheDocument();
+    expect(screen.getByText('Generate Manually')).toBeInTheDocument();
+  });
+});
