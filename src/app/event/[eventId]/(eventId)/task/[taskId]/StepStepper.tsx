f86d0d840145be638ab49766ba5bb2b87ffe27ef
JohannesSetiawan <johannes.setiawan@ui.ac.id>
2024-04-01 19:11:22 +0700

    [REFACTOR] Fix UI on step

--- src/app/event/[eventId]/(eventId)/task/[taskId]/StepStepper.tsx
+++ src/app/event/[eventId]/(eventId)/task/[taskId]/StepStepper.tsx
@@ -103,7 +103,7 @@ export default function StepStepper({
     <div className="flex flex-col gap-8 px-5 py-3 lg:px-10 lg:py-6 border border-teal-600 rounded-xl">
         <div className="flex flex-row gap-3">
         <i className="i-ph-list-checks-light size-6 text-gray-950" />
-            <p>Step {activeStep} out of {steps?.length}</p>
+            <p>step {Math.min(activeStep+1, steps?.length)} out of {steps?.length}</p>
         </div>
         <Stepper activeStep={activeStep} orientation="vertical">
             {steps?.map((step, index) => (
