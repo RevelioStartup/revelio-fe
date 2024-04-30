cf522704f74889a64fed1bde1f97cc6cea7ba92a
emiriko <notalvaroaustin@gmail.com>
2024-04-30 13:08:12 +0700

    chore: formatting

--- src/app/profile/SubscriptionHistory.tsx
+++ src/app/profile/SubscriptionHistory.tsx
@@ -4,20 +4,18 @@ import React from 'react'
 
 export const SubscriptionHistory: React.FC<{
   data: SubscriptionHistoryResponse[]
-}> = ({
-  data
-}) => {
+}> = ({ data }) => {
   return (
     <Box
-        component="section"
-        className="flex flex-wrap gap-3 w-full overflow-y-auto h-[calc(100vh-80px)]"
-        padding={{ xs: '2em 2em', lg: '4em 6em' }}
-      >
-        <h2 className="text-3xl md:text-5xl text-center w-full font-bold">
-          Subscription History
-        </h2>
-        {data.map((history) => (
-          <Box
+      component="section"
+      className="flex flex-wrap gap-3 w-full overflow-y-auto h-[calc(100vh-80px)]"
+      padding={{ xs: '2em 2em', lg: '4em 6em' }}
+    >
+      <h2 className="text-3xl md:text-5xl text-center w-full font-bold">
+        Subscription History
+      </h2>
+      {data.map((history) => (
+        <Box
           key={history.id}
           sx={{
             display: 'flex',
@@ -46,10 +44,13 @@ export const SubscriptionHistory: React.FC<{
             </h3>
           </Box>
           <Box sx={{ flexGrow: 0, mb: '0.5rem', color: 'white' }}>
-            <p style={{ margin: 0 }}>{new Date(history.start_date).toDateString()} - {new Date(history.end_date).toDateString()} </p>
+            <p style={{ margin: 0 }}>
+              {new Date(history.start_date).toDateString()} -{' '}
+              {new Date(history.end_date).toDateString()}{' '}
+            </p>
           </Box>
         </Box>
-        ))}
+      ))}
     </Box>
   )
 }
