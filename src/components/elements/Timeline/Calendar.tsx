b04a8e7b3ef463537237e9d1d4eb9d0ae1280d08
emiriko <notalvaroaustin@gmail.com>
2024-05-07 00:20:16 +0700

    [GREEN] Finish all fixing part

--- src/components/elements/Timeline/Calendar.tsx
+++ src/components/elements/Timeline/Calendar.tsx
@@ -5,11 +5,14 @@ import interactionPlugin from '@fullcalendar/interaction'
 import Modal from '@mui/material/Modal'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
-import { useGetTimelinesByEventQuery, useModifyDetailTimelineMutation } from '@/redux/api/timelineApi'
+import {
+  useGetTimelinesByEventQuery,
+  useModifyDetailTimelineMutation,
+} from '@/redux/api/timelineApi'
 import { EventApi } from '@fullcalendar/core/index.js'
 import FullCalendar from '@fullcalendar/react'
 import { Button } from '../Button'
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 import dayjs, { Dayjs } from 'dayjs'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers'
@@ -24,13 +27,14 @@ const DemoApp: React.FC<DemoAppProps> = ({ eventId }) => {
     event_id: eventId,
   })
   const [showModal, setShowModal] = React.useState(false)
-  const [ isEditing, setIsEditing ] = React.useState(false)
-  const [ newStartDate, setNewStartDate ] = React.useState<Dayjs | null>(null)
-  const [ newEndDate, setNewEndDate ] = React.useState<Dayjs | null>(null)
+  const [isEditing, setIsEditing] = React.useState(false)
+  const [newStartDate, setNewStartDate] = React.useState<Dayjs | null>(null)
+  const [newEndDate, setNewEndDate] = React.useState<Dayjs | null>(null)
 
   const [clickedEvent, setClickedEvent] = React.useState<EventApi | null>(null)
 
-  const [ editDetailTimeline, { isLoading: modifyDetailTimelineLoading }] = useModifyDetailTimelineMutation()
+  const [editDetailTimeline, { isLoading: modifyDetailTimelineLoading }] =
+    useModifyDetailTimelineMutation()
 
   const handleEventClick = (clickInfo: any) => {
     setShowModal(true)
@@ -70,14 +74,12 @@ const DemoApp: React.FC<DemoAppProps> = ({ eventId }) => {
     p: 4,
   }
 
-  
-
   if (isLoading) {
     return (
       <div className="flex flex-col justify-center items-center min-h-[90vh]">
         <div data-testid="loader" className="loader"></div>
       </div>
-    );
+    )
   }
 
   return (
@@ -116,53 +118,72 @@ const DemoApp: React.FC<DemoAppProps> = ({ eventId }) => {
               <Typography id="modal-modal-description" sx={{ mt: 2 }}>
                 End date: {clickedEvent?.end?.toLocaleDateString()}
               </Typography>
-              <Button className='mt-4' onClick={() => setIsEditing(true)}>
+              <Button className="mt-4" onClick={() => setIsEditing(true)}>
                 Edit Event
               </Button>
             </Box>
           ) : (
-            <Box sx = {style}> 
+            <Box sx={style}>
               <Typography id="modal-modal-description" sx={{ mt: 2 }}>
                 Start Date
               </Typography>
-              <Box sx = {{ mt: 2}}>
-                <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='id'>       
-                  <DatePicker label="Start Date" value={newStartDate}
-                  onChange={(newValue) => setNewStartDate(newValue)}
-                  slotProps={{ textField: { required: true } }}
+              <Box sx={{ mt: 2 }}>
+                <LocalizationProvider
+                  dateAdapter={AdapterDayjs}
+                  adapterLocale="id"
+                >
+                  <DatePicker
+                    label="Start Date"
+                    value={newStartDate}
+                    onChange={(newValue) => setNewStartDate(newValue)}
+                    slotProps={{ textField: { required: true } }}
                   />
                 </LocalizationProvider>
               </Box>
               <Typography id="modal-modal-description" sx={{ mt: 2 }}>
                 End Date
               </Typography>
-              <Box sx = {{ mt: 2}}>
-                <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='id'>       
-                  <DatePicker label="End Date"
-                  value={newEndDate}
-                  onChange={(newValue) => setNewEndDate(newValue)}
-                  slotProps={{ textField: { required: true } }}
+              <Box sx={{ mt: 2 }}>
+                <LocalizationProvider
+                  dateAdapter={AdapterDayjs}
+                  adapterLocale="id"
+                >
+                  <DatePicker
+                    label="End Date"
+                    value={newEndDate}
+                    onChange={(newValue) => setNewEndDate(newValue)}
+                    slotProps={{ textField: { required: true } }}
                   />
                 </LocalizationProvider>
               </Box>
-              <Box sx={{ display: "flex", gap: 8, mt: 2 }}>
-                <LoadingButton disabled = {modifyDetailTimelineLoading} onClick={() => setIsEditing(false)}>
+              <Box sx={{ display: 'flex', gap: 8, mt: 2 }}>
+                <LoadingButton
+                  disabled={modifyDetailTimelineLoading}
+                  onClick={() => setIsEditing(false)}
+                >
                   Cancel
                 </LoadingButton>
-                <LoadingButton onClick={async () => {
-                  const cleanedStartDatetime = newStartDate?.toISOString().split('T')[0] as string
-                  const cleanedEndDatetime = newEndDate?.toISOString().split('T')[0] as string
-                  
-                  await editDetailTimeline({
-                    id: clickedEvent?.id as string,
-                    start_datetime: cleanedStartDatetime,
-                    end_datetime: cleanedEndDatetime
-                  })
-                
-                  setShowModal(false)
-                  setIsEditing(false)
-                }} loading={modifyDetailTimelineLoading}
-                loadingIndicator={'Editing...'} >
+                <LoadingButton
+                  onClick={async () => {
+                    const cleanedStartDatetime = newStartDate
+                      ?.toISOString()
+                      .split('T')[0] as string
+                    const cleanedEndDatetime = newEndDate
+                      ?.toISOString()
+                      .split('T')[0] as string
+
+                    await editDetailTimeline({
+                      id: clickedEvent?.id as string,
+                      start_datetime: cleanedStartDatetime,
+                      end_datetime: cleanedEndDatetime,
+                    })
+
+                    setShowModal(false)
+                    setIsEditing(false)
+                  }}
+                  loading={modifyDetailTimelineLoading}
+                  loadingIndicator={'Editing...'}
+                >
                   Save
                 </LoadingButton>
               </Box>
