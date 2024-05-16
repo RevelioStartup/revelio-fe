f3d3387e3eda7b4d2fde09743535cd8598323b29
Catherine Angel <catheriner40540@gmail.com>
2024-05-16 14:08:49 +0700

    [RED] update test suited for payment status page

--- __tests__/payment/payment-fail.test.tsx
+++ __tests__/payment/payment-fail.test.tsx
@@ -67,6 +67,4 @@ describe('PaymentSuccess Component', () => {
       )
     })
   })
-
-
 })
--- src/app/payment/success/page.tsx
+++
@@ -1,4 +0,0 @@
-import { PaymentSuccess } from './PaymentSuccess'
-export default function PaymentSuccessPage() {
-  return <PaymentSuccess />
-}
