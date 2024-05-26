a37c0936d5ceb3a3790918cc0e61f4dac1f52ae4
emiriko <notalvaroaustin@gmail.com>
2024-05-26 15:42:03 +0700

    [REFACTOR] Refactor layouts

--- __tests__/dashboard/dashboard_layout.tsx
+++ __tests__/dashboard/dashboard_layout.tsx
@@ -4,15 +4,14 @@ import DashboardLayout from '../../src/app/dashboard/layout'
 import '@testing-library/jest-dom'
 
 describe('Package Layout Component', () => {
-    it('renders the layout with Navbar, Footer, and children', () => {
-      const childText = 'Child Content'
-      render(
-        <DashboardLayout>
-          <div>{childText}</div>
-        </DashboardLayout>
-      )
-  
-      expect(screen.getByText(childText)).toBeInTheDocument()
-    })
+  it('renders the layout with Navbar, Footer, and children', () => {
+    const childText = 'Child Content'
+    render(
+      <DashboardLayout>
+        <div>{childText}</div>
+      </DashboardLayout>
+    )
+
+    expect(screen.getByText(childText)).toBeInTheDocument()
+  })
 })
-  
--- __tests__/package/package_layout.tsx
+++ __tests__/package/package_layout.tsx
@@ -4,15 +4,14 @@ import PackageLayout from '../../src/app/package/layout'
 import '@testing-library/jest-dom'
 
 describe('Package Layout Component', () => {
-    it('renders the layout with Navbar, Footer, and children', () => {
-      const childText = 'Child Content'
-      render(
-        <PackageLayout>
-          <div>{childText}</div>
-        </PackageLayout>
-      )
-  
-      expect(screen.getByText(childText)).toBeInTheDocument()
-    })
+  it('renders the layout with Navbar, Footer, and children', () => {
+    const childText = 'Child Content'
+    render(
+      <PackageLayout>
+        <div>{childText}</div>
+      </PackageLayout>
+    )
+
+    expect(screen.getByText(childText)).toBeInTheDocument()
   })
-  
+})
